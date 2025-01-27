c8cd0c5d7ceb0eca30f960b2e83f82047c2ea10d
kengomasunari2025 <kengomasunari2025@gmail.com>
2025-01-27 20:56:03 +0900

    最初のプッシュ

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Kengo from "./React/Kengo";
+
+const App = () => {
+  return (
+    <>
+      <Kengo />
+    </>
+  );
+};
+
+export default App;
---
+++ src/React/Kengo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SlickSlider from "./SlickSlider/SlickSlider";
+import Footer from "./Footer/Footer";
+// git clone https://github.com/kengomasunari2025/20250128.git
+// cd 20250128
+// git config --global user.name "Your Name"
+// git config --global user.email "your_email@example.com"
+// git push origin main
+// git pull origin main
+
+const Kengo = () => {
+  return (
+    <div className="bg-white">
+      <SlickSlider />
+      <Footer />
+    </div>
+  );
+};
+export default Kengo;
